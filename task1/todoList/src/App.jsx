0ec0aa987c78451d27548858b19bca639a0aaa98
hmx <1037107465@qq.com>
2024-06-24 20:03:40 +0800

    adjust the property of objects

--- task1/todoList/src/App.jsx
+++ task1/todoList/src/App.jsx
@@ -3,25 +3,31 @@ import Header from "./components/Header";
 // import TodoItem from "./components/ToDoItem";
 import AddTodo from "./components/AddToDo";
 import TodoList from "./components/ToDoList";
-import { useState } from "react";
-
+import { useEffect, useState } from "react";
+// import PropTypes from "prop-types";
 function App() {
-  // const [whole, setWhole] = useState([]);
   const [whole, setWhole] = useState([]);
+  const generateId = () => Math.random().toString(36).substr(2, 9);
   const addItem = (newItem) => {
-    setWhole([...whole, newItem]);
+    const newer = { id: generateId(), todo: newItem }; //定义新对象形式，等会儿删除的时候根据id来删除
+    setWhole([...whole, newer]);
     console.log(whole);
   };
-  const deleteItem = (index) => {
-    const newWhole = whole.filter((i) => i !== index);
+  const handleDeleteItem = (id) => {
+    const newWhole = whole.filter((item) => item.id !== id);
+    console.log("yo", newWhole);
+    console.log("Deleted item at index:", id);
     setWhole(newWhole);
   };
+  useEffect(() => {
+    console.log("Updated todos:", whole);
+  }, [whole]);
   return (
     <>
       <div className="app">
         <Header />
         <AddTodo addItem={addItem} />
-        <TodoList todos={whole} delete={deleteItem} />
+        <TodoList todos={whole} onDelete={handleDeleteItem} />
       </div>
     </>
   );
--- task1/todoList/src/components/ToDoList/index.jsx
+++ task1/todoList/src/components/ToDoList/index.jsx
@@ -1,16 +1,27 @@
 // import React from "react";
 import TodoItem from "../ToDoItem";
 import PropTypes from "prop-types";
-const TodoList = ({ todos }) => {
+const TodoList = ({ todos, onDelete }) => {
   return (
     <div>
-      {todos.map((todo, index) => (
-        <TodoItem key={index} todo={todo} />
+      {todos.map((item) => (
+        <TodoItem
+          key={item.id}
+          todo={item.todo}
+          clickToDelete={onDelete}
+          id={item.id}
+        />
       ))}
     </div>
   );
 };
 TodoList.propTypes = {
-  todos: PropTypes.arrayOf(PropTypes.string).isRequired,
+  todos: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      todo: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+  onDelete: PropTypes.func.isRequired,
 };
 export default TodoList;
